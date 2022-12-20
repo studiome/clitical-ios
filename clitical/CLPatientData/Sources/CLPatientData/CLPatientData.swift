060ece4f2c34009e367909f15228d304bfa69799
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-21 00:20:39 +0900

    add CKD

--- clitical/CLPatientData/Sources/CLPatientData/CLPatientData.swift
+++ clitical/CLPatientData/Sources/CLPatientData/CLPatientData.swift
@@ -5,9 +5,11 @@ public struct PatientData {
     public var height:Double?
     public var weight:Double?
     public var activity:Activity
+    public var ckd: CKD
     public init() {
         sex = Sex.Female;
         activity = Activity.Ambulatory;
+        ckd = CKD.Normal
     }
 }
 
@@ -20,4 +22,12 @@ public enum Activity{
     case Ambulatory
     case Wheelchair
     case Immobile
+}
+
+public enum CKD{
+    case Normal
+    case G3
+    case G4
+    case G5
+    case G5D
 }
--- clitical/CLPatientData/Tests/CLPatientDataTests/CLPatientDataTests.swift
+++ clitical/CLPatientData/Tests/CLPatientDataTests/CLPatientDataTests.swift
@@ -2,12 +2,13 @@ import XCTest
 @testable import CLPatientData
 
 final class PatientDataTests: XCTestCase {
-    func testExample() throws {
+    func testInit() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         let pd = PatientData();
         XCTAssertEqual(pd.sex, Sex.Female);
         XCTAssertEqual(pd.activity, Activity.Ambulatory);
+        XCTAssertEqual(pd.ckd,CKD.Normal);
     }
 }
