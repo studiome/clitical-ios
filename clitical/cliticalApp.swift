72986a65b744557e4580be3fd9e6c822d824ad8d
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-20 18:10:26 +0900

    Initial Commit

---
+++ clitical/cliticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  cliticalApp.swift
+//  clitical
+//
+//  Created by kmiyahara on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct cliticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
