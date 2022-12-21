08b10ce2eafb9eaefb724ad204447f1c674dfb25
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-21 16:34:43 +0900

    add question properties

--- clitical/CLPatientData/Sources/CLPatientData/CLPatientData.swift
+++ clitical/CLPatientData/Sources/CLPatientData/CLPatientData.swift
@@ -6,11 +6,44 @@ public struct CLPatientData {
     public var weight:Double?
     public var alb:Double?
     public var activity:Activity
+    public var hasCHF: Bool
+    public var hasCAD: Bool
+    public var hasCVD: Bool
     public var ckd: CKD
+    public var malignantNeoplasm: MalignantNeoplasm
+    public var hasAILesion: Bool
+    public var hasFPLesion: Bool
+    public var hasBKLesion: Bool
+    public var isUrgent: Bool
+    public var hasFever: Bool
+    public var hasAbnormalWBC: Bool
+    public var hasLocalInfection: Bool
+    public var hasDyslipidemia: Bool
+    public var isSmoking: Bool
+    public var hasContraLateralLesion: Bool
+    public var hasOtherVD: Bool
+    public var rutherford: RutherfordClassification
+    
     public init() {
         sex = Sex.Female;
         activity = Activity.Ambulatory;
-        ckd = CKD.Normal
+        hasCHF = false;
+        hasCAD = false;
+        hasCVD = false;
+        ckd = .Normal;
+        malignantNeoplasm = .No;
+        hasAILesion = false;
+        hasFPLesion = false;
+        hasBKLesion = false;
+        isUrgent = false;
+        hasFever = false;
+        hasAbnormalWBC = false;
+        hasLocalInfection = false;
+        hasDyslipidemia = false;
+        isSmoking = false;
+        hasContraLateralLesion = false;
+        hasOtherVD = false;
+        rutherford = .Class4;
     }
 }
 
@@ -31,4 +64,16 @@ public enum CKD{
     case G4
     case G5
     case G5D
+}
+
+public enum MalignantNeoplasm{
+    case No
+    case PastHistory
+    case UnderTreatment
+}
+
+public enum RutherfordClassification{
+    case Class4
+    case Class5
+    case Class6
 }
--- clitical/CLPatientData/Tests/CLPatientDataTests/CLPatientDataTests.swift
+++ clitical/CLPatientData/Tests/CLPatientDataTests/CLPatientDataTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class CLPatientDataTests: XCTestCase {
     func testInit() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
         let pd = CLPatientData();
         XCTAssertEqual(pd.sex, Sex.Female);
         XCTAssertNil(pd.age);
@@ -13,6 +10,22 @@ final class CLPatientDataTests: XCTestCase {
         XCTAssertNil(pd.weight);
         XCTAssertNil(pd.alb);
         XCTAssertEqual(pd.activity, Activity.Ambulatory);
+        XCTAssertFalse(pd.hasCHF);
+        XCTAssertFalse(pd.hasCAD);
+        XCTAssertFalse(pd.hasCVD);
         XCTAssertEqual(pd.ckd,CKD.Normal);
+        XCTAssertEqual(pd.malignantNeoplasm, MalignantNeoplasm.No);
+        XCTAssertFalse(pd.hasAILesion);
+        XCTAssertFalse(pd.hasFPLesion);
+        XCTAssertFalse(pd.hasBKLesion);
+        XCTAssertFalse(pd.isUrgent);
+        XCTAssertFalse(pd.hasFever);
+        XCTAssertFalse(pd.hasAbnormalWBC);
+        XCTAssertFalse(pd.hasLocalInfection);
+        XCTAssertFalse(pd.hasDyslipidemia);
+        XCTAssertFalse(pd.isSmoking);
+        XCTAssertFalse(pd.hasContraLateralLesion);
+        XCTAssertFalse(pd.hasOtherVD);
+        XCTAssertEqual(pd.rutherford, RutherfordClassification.Class4);
     }
 }
