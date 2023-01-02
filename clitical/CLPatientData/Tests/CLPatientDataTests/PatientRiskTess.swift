2bb011441130bd3a9698621e63533c3877473f82
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2023-01-02 10:55:44 +0900

    init test for risk

---
+++ clitical/CLPatientData/Tests/CLPatientDataTests/PatientRiskTess.swift
@@ -0,0 +1,21 @@
+//
+//  PatientRiskTest.swift
+//  
+//
+//  Created by kmiyahara on 2023/01/02.
+//
+
+import XCTest
+@testable import CLPatientData
+final class PatientRiskTests: XCTestCase {
+    func testInit() throws {
+        let risk = PatientRisk();
+        XCTAssertNil(risk.gnri);
+        XCTAssertNil(risk.gnriRisk);
+        XCTAssertNil(risk.predicted30DDeathOrAmputation);
+        XCTAssertNil(risk.predicted30DMALE);
+        XCTAssertNil(risk.predicted2YOS);
+        XCTAssertNil(risk.predicted2YOSRisk);
+        XCTAssertNil(risk.predicted2YAFS);
+    }
+}
