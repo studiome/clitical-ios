f426fc891370cdf48b4c0428f88149b450293450
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2023-01-25 14:11:09 +0900

    CAD

--- clitical/CADChoiceView.swift
+++ clitical/CADChoiceView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 import CLPatientData
 
 struct CADChoiceView: View {
-    
+    @EnvironmentObject var patientData: PatientData
     var body: some View {
-        Text("CADQuestionDescription")
+        List{
+            Section(footer: Text("CADQuestionDescription")){
+                ForEach(YesNo.allCases, id: \.self){item in
+                    HStack{
+                        Text(LocalizedStringKey(item.label))
+                        Spacer()
+                        if(patientData.hasCAD == item.toBool()){
+                            Image(systemName: "checkmark")
+                                .foregroundColor(jsvsColor)
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        patientData.hasCAD = item.toBool()
+                    }
+                }
+            }
+        }
+        .navigationTitle("CADQuestionTitle")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
