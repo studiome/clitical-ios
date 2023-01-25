0b607e20104c006c47078dc8c562e3bbc7c178fb
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2023-01-25 13:46:31 +0900

    description check

--- clitical/CKDChoiceView.swift
+++ clitical/CKDChoiceView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CKDChoiceView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("CKDQuestionDescription")
     }
 }
 
